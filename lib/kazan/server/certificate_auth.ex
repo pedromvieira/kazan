5c3d8274b407640ad485b2cc9713b10ca52fb57f
Pedro Vieira <pedro@vieira.net>
2022-11-02 10:40:21 -0300

    Update certificate_auth.ex

--- lib/kazan/server/certificate_auth.ex
+++ lib/kazan/server/certificate_auth.ex
@@ -6,13 +6,13 @@ defmodule Kazan.Server.CertificateAuth do
           key: binary
         }
 
-  defimpl Inspect, for: __MODULE__ do
-    # We define a custom inspect implementation to avoid printing key details to
-    # logs or anything.
-    import Inspect.Algebra
+  # defimpl Inspect, for: __MODULE__ do
+  #   # We define a custom inspect implementation to avoid printing key details to
+  #   # logs or anything.
+  #   import Inspect.Algebra
 
-    def inspect(_auth, _opts) do
-      concat(["#CertificateAuth<...>"])
-    end
-  end
+  #   def inspect(_auth, _opts) do
+  #     concat(["#CertificateAuth<...>"])
+  #   end
+  # end
 end
